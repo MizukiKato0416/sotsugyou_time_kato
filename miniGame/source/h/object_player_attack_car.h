33267e8829d230fbf050c8017a6b68f9ed12f2db
ToshikiTsuruma <201020@st.yoshida-g.jp.ac>
2022-11-02 14:09:16 +0900

    プレイヤーの後ろ移動のガタガタ修復

--- miniGame/source/h/object_player_attack_car.h
+++ miniGame/source/h/object_player_attack_car.h
@@ -69,7 +69,6 @@ private:
 	void CollisionObjectPlayer(void);				//プレイヤーとの当たり判定
 
 	D3DXVECTOR3	m_lastPos;			//最後の位置座標
-	D3DXVECTOR3 m_destRot;			//目標の角度
 	D3DXVECTOR3 m_move;				//移動量
 	D3DXVECTOR3 m_boundMove;		//バウンド移動量
 	float m_fMoveSpeed;				//移動する速さ
