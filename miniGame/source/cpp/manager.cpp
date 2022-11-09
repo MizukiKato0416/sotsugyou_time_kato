2399c06ccec81195df18ca6215b42e330ba56caf
mimimimimi0416 <201014@st.yoshida-g.ac.jp>
2022-11-09 13:12:31 +0900

    Texture

--- miniGame/source/cpp/manager.cpp
+++ miniGame/source/cpp/manager.cpp
@@ -131,7 +131,7 @@ HRESULT CManager::Init(HINSTANCE hInstance, HWND hWnd, bool bWindow) {
 	CSphere::CreateTextureSphere();
 
 	//ÉVÅ[ÉìÇÃê∂ê¨
-	CScene::ChangeScene(m_pScene, CScene::SCENE_TYPE::GAME_02);
+	CScene::ChangeScene(m_pScene, CScene::SCENE_TYPE::SELECT_GAME);
 
 	return S_OK;
 }
