7294b6e92af08db46b2233721ea3e138c1dbf63b
satouryo-hei <201011@st.yoshida-g.ac.jp>
2022-11-25 19:06:46 +0900

    {}ついてないところあったので付けておいた

--- miniGame/source/h/titleScene.h
+++ miniGame/source/h/titleScene.h
@@ -39,14 +39,14 @@ private:
 	void PlayerModel();			//プレイヤーのモデルの処理
 	void Camera();				//カメラの処理
 
-	static CObject2D * m_pNext;// オブジェクト2Dの静的ポインター生成
-	const int m_nDivideNum;	// 割る数
+	static CObject2D * m_pNext;	// オブジェクト2Dの静的ポインター生成
+	const int m_nDivideNum;		// 割る数
 	const int m_nMaxColTime;	// 色が変わる時間の最大値
-	const float m_fMaxCol;	// 色の最大値
-	int m_nFadeTime;	// 遷移する時間	
-	int m_nColorTime;	// 色を変える時間	
-	bool m_bCol; // 色を変えるかどうか
-	bool m_bPushKey; // 押されたかどうか
+	const float m_fMaxCol;		// 色の最大値
+	int m_nFadeTime;			// 遷移する時間	
+	int m_nColorTime;			// 色を変える時間	
+	bool m_bCol;				// 色を変えるかどうか
+	bool m_bPushKey;			// 押されたかどうか
 
 	std::vector<CMeshwall*> m_pCloud;							//雲のポインタ
 	CObjectModel *m_pPlayerModel[MAX_OBJECT_PLAYER_NUM];		//プレイヤーのモデル
